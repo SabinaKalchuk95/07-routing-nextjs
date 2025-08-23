e9e57112f09ddf5a18e4fad2401a8472ddb37e30
Sabina KAlchuk <ssabki229@gmail.com>
2025-08-23 13:45:12 +0200

    fixfilterlayout

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -3,16 +3,16 @@ import css from "./LayoutNotes.module.css";
 type Props = {
   children: React.ReactNode;
   sidebar: React.ReactNode;
-  modal?: React.ReactNode; // optional
+ 
 };
 
-const NotesLayout = ({ children, sidebar, modal }: Props) => {
+const NotesLayout = ({ children, sidebar,}: Props) => {
   return (
     <div className={css.container}>
       <aside className={css.sidebar}>{sidebar}</aside>
       <main className={css.notesWrapper}>{children}</main>
-      {modal && <div className={css.modalWrapper}>{modal}</div>}
-    </div>
+      </div>
+    
   );
 };
 
