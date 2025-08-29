669364fae9109aaa3f42e3e4746ad0344f23773a
Sabina KAlchuk <ssabki229@gmail.com>
2025-08-29 19:52:17 +0200

    4321

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,41 +1,37 @@
 import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
 import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
 } from '@tanstack/react-query';
 
-interface Props {
-  params: {
-    slug: string[];
-  };
-}
-
 export default async function NotesByTags({
-  params,
-}: Props) {
-  const initialPage = 1;
-  const initialSearch = '';
-  const perPage = 12;
-  const tag = params.slug?.[0] === 'All' ? undefined : params.slug?.[0];
+  params,
+}: { params: Promise<{ slug: string[] }> }) {
+  const { slug } = await params;
 
-  const queryClient = new QueryClient();
+  const initialPage = 1;
+  const initialSearch = '';
+  const perPage = 12;
+  const tag = slug?.[0] === 'All' ? undefined : slug?.[0];
 
-  await queryClient.prefetchQuery({
-    queryKey: ['notes', { search: initialSearch, page: initialPage, perPage, tag }],
-    queryFn: () =>
-      fetchNotes({
-        search: initialSearch,
-        page: initialPage,
-        perPage,
-        tag,
-      }),
-  });
+  const queryClient = new QueryClient();
 
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotesClient tag={tag} />
-    </HydrationBoundary>
-  );
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', { search: initialSearch, page: initialPage, perPage, tag }],
+    queryFn: () =>
+      fetchNotes({
+        search: initialSearch,
+        page: initialPage,
+        perPage,
+        tag,
+      }),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient tag={tag} />
+    </HydrationBoundary>
+  );
 }
